714052985e42328bfc32b9d4f506340a3d03dbe3
PARAMESH T S <parameshroboticist@gmail.com>
2019-08-13 22:38:22 +0530

    Finding DNA Samples

---
+++ Finding_gene_in_DNA.java
@@ -0,0 +1,51 @@
+
+/**
+ * Write a description of Finding_gene_in_DNA here.
+ * 
+ * @author (Paramesh) 
+ * @version (13-Aug-2019)
+ */
+
+import java.io.*;
+
+public class Finding_gene_in_DNA {
+  public String findSimpleGene(String dna){
+    String result = ""; 
+    int startindex = dna.indexOf("ATG");
+    if(startindex==-1){
+        return "";
+    }
+    int stopindex = dna.indexOf("TAA",startindex+3);
+    if(stopindex==-1){
+        return "";
+    }
+    if((stopindex - startindex) % 3 == 0) {
+            return dna.substring(startindex, stopindex+3);
+        }
+        else {
+            return "";
+        }
+    }
+    
+    public void testSimpleGene ()
+    {String a = "ATDATGASDEDGHTATAA";
+      String a1 = "ASDEDGHTATAA";
+      String a2 = "ATDATGASDEDGHTA";
+      String a3 = "ATDATGASDEDGHTTAA";
+      String a4 = "ATDASDEDGHT";
+      
+     
+   System.out.println("The string is: " + a + ". The Gene is: " + findSimpleGene(a));
+   System.out.println("The string is: " + a1 + ". The Gene is: " + findSimpleGene(a1));
+   System.out.println("The string is: " + a2 + ". The Gene is: " + findSimpleGene(a2));
+    System.out.println("The string is: " + a3 + ". The Gene is: " + findSimpleGene(a3));
+    System.out.println("The string is: " + a4 + ". The Gene is: " + findSimpleGene(a4));
+    
+    
+    }
+    public static void main(String[] args)
+    {
+        Part1 obj = new Part1();
+        obj.testSimpleGene();
+    }
+}
